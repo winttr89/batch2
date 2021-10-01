6f3571790582378382da70a54b9de2954e7d1cbb
winttr89 <89454761+winttr89@users.noreply.github.com>
2021-10-01 08:22:43 +0000

    Update auto1.sh

--- auto1.sh
+++ auto1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sub="1df2310e-0dba-488b-9be9-2c9e8b638465"
+sub="24dbe831-12ce-421a-99f0-420c2d243efd"
 ran=`head /dev/urandom | tr -dc a-z0-9 | fold -w 3 | head -n 1`
 wget -O batch.json https://raw.githubusercontent.com/winttr89/batch2/main/batch.json
 wget -O batch2.json https://raw.githubusercontent.com/winttr89/batch2/main/batch2.json
