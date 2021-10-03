3f3144f219037dd601340337c720ecd3e0dd0d6b
winttr89 <89454761+winttr89@users.noreply.github.com>
2021-10-03 10:21:30 +0000

    Update auto4.sh

--- auto4.sh
+++ auto4.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sub="cfca14da-62f6-45c9-bea6-ebf4e39713dd"
+sub="345f4ec3-6e04-420e-b20a-3a4f9abfe6a4"
 ran=`head /dev/urandom | tr -dc a-z0-9 | fold -w 3 | head -n 1`
 wget -O batch.json https://raw.githubusercontent.com/winttr89/batch2/main/batch.json
 wget -O batch2.json https://raw.githubusercontent.com/winttr89/batch2/main/batch2.json
